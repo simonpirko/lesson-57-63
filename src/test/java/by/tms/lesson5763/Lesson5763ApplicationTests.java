1fe0d2e4eb0e758455c45757df04ca4b3082a7d7
simonpirko <sipi7gm@gmail.com>
2023-06-27 19:58:36 +0300

    Change

--- src/test/java/by/tms/lesson5763/Lesson5763ApplicationTests.java
+++ src/test/java/by/tms/lesson5763/Lesson5763ApplicationTests.java
@@ -1,13 +1,18 @@
 package by.tms.lesson5763;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 class Lesson5763ApplicationTests {
 
   @Test
   void contextLoads() {
+    fail();
   }
 
 }
